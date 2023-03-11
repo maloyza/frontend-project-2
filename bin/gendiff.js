5aa47f2dfff8fd470bd7529dbcee1ac2b64be830
maloyza <maloyza211@mail.ru>
2023-03-11 18:31:48 +0400

    added the format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,8 @@ import { program } from 'commander';
 program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
-    .option('-h, --help', 'display help for command');
+    .option('-h, --help', 'display help for command')
+    .option('-f, --format <type>', 'output format');
 
 program.on('option:help', () => {
     console.log(program.helpInformation());
